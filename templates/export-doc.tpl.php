d27bd6d07a7a3e00f74a09d9b440b02b7eb20e96
Timo Epp <timo.epp@vzbv.de>
2016-01-21 15:25:13 +0100

    Initial commit

---
+++ templates/export-doc.tpl.php
@@ -0,0 +1,24 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<style>
+  body, pre {font-family: Helvetica;}
+  h1 {font-size: 1.5em;}
+  h2 {font-size: 1.1em;}
+  label {font-weight: bold;}
+  .field-label { font-weight: bold; }
+  .field-label-inline { display: none;}
+  .field-label-inline-first { font-weight: bold;}
+  .field-type-datestamp div { display: inline;}
+  .field-type-number-float div { display: inline;}
+  .field-type-content-taxonomy div { display: inline;}
+  .field-type-text div { display: inline;}
+  .print-footnote,
+  .group-buttons,
+  .rules-link-wrapper,
+  .element-invisible,
+  .field-group-htabs-wrapper {display:none;}
+</style>
+<body>
+<?php print $content;?>
+</body>
+</html>
